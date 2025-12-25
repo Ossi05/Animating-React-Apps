aaf20313d638387441b3f4c43cfa928a06348287
Ossi05 <77546709+Ossi05@users.noreply.github.com>
2025-12-25 16:34:18 +0200

    Update NewChallenge component's image selection animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,9 +25,7 @@ export default function Header() {
         <motion.button
           onClick={handleStartAddNewChallenge}
           className="button"
-          whileHover={{
-            scale: 1.1,
-          }}
+          whileHover={{ scale: 1.1 }}
           transition={{ type: "spring", stiffness: 500 }}
         >
           Add Challenge
