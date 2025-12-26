d3856d0245e073a57dbc873b2bb810b325c0cfd1
Ossi05 <77546709+Ossi05@users.noreply.github.com>
2025-12-26 15:22:29 +0200

    Animate Challenge list items

--- src/components/Challenges.jsx
+++ src/components/Challenges.jsx
@@ -1,12 +1,12 @@
-import { useContext, useState } from 'react';
-
-import { ChallengesContext } from '../store/challenges-context.jsx';
-import ChallengeItem from './ChallengeItem.jsx';
-import ChallengeTabs from './ChallengeTabs.jsx';
+import { useContext, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { ChallengesContext } from "../store/challenges-context.jsx";
+import ChallengeItem from "./ChallengeItem.jsx";
+import ChallengeTabs from "./ChallengeTabs.jsx";
 
 export default function Challenges() {
   const { challenges } = useContext(ChallengesContext);
-  const [selectedType, setSelectedType] = useState('active');
+  const [selectedType, setSelectedType] = useState("active");
   const [expanded, setExpanded] = useState(null);
 
   function handleSelectType(newType) {
@@ -14,7 +14,7 @@ export default function Challenges() {
   }
 
   function handleViewDetails(id) {
-    setExpanded((prevId) => {
+    setExpanded(prevId => {
       if (prevId === id) {
         return null;
       }
@@ -24,11 +24,9 @@ export default function Challenges() {
   }
 
   const filteredChallenges = {
-    active: challenges.filter((challenge) => challenge.status === 'active'),
-    completed: challenges.filter(
-      (challenge) => challenge.status === 'completed'
-    ),
-    failed: challenges.filter((challenge) => challenge.status === 'failed'),
+    active: challenges.filter(challenge => challenge.status === "active"),
+    completed: challenges.filter(challenge => challenge.status === "completed"),
+    failed: challenges.filter(challenge => challenge.status === "failed"),
   };
 
   const displayedChallenges = filteredChallenges[selectedType];
@@ -40,19 +38,35 @@ export default function Challenges() {
         onSelectType={handleSelectType}
         selectedType={selectedType}
       >
-        {displayedChallenges.length > 0 && (
-          <ol className="challenge-items">
-            {displayedChallenges.map((challenge) => (
-              <ChallengeItem
-                key={challenge.id}
-                challenge={challenge}
-                onViewDetails={() => handleViewDetails(challenge.id)}
-                isExpanded={expanded === challenge.id}
-              />
-            ))}
-          </ol>
-        )}
-        {displayedChallenges.length === 0 && <p>No challenges found.</p>}
+        <AnimatePresence mode="wait">
+          {displayedChallenges.length > 0 && (
+            <motion.ol
+              key="list"
+              exit={{ y: -30, opacity: 0 }}
+              className="challenge-items"
+            >
+              {displayedChallenges.map(challenge => (
+                <AnimatePresence key={challenge.id}>
+                  <ChallengeItem
+                    challenge={challenge}
+                    onViewDetails={() => handleViewDetails(challenge.id)}
+                    isExpanded={expanded === challenge.id}
+                  />
+                </AnimatePresence>
+              ))}
+            </motion.ol>
+          )}
+          {displayedChallenges.length === 0 && (
+            <motion.p
+              key="fallback"
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -20 }}
+            >
+              No challenges found.
+            </motion.p>
+          )}
+        </AnimatePresence>
       </ChallengeTabs>
     </div>
   );
